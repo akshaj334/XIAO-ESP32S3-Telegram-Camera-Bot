6b1ab42050a1c6fa0e94eac2e8be0628727800d0
akshaj334 <akshaj334@gmail.com>
2026-06-15 14:40:21 +0530

    secrets.h

--- secrets.h
+++ secrets.h
@@ -1,4 +1,7 @@
-const char* DEFAULT_SSID
-const char* DEFAULT_PASSWORD
-const char* BOT_TOKEN
-const char* CHAT_ID
+#pragma once
+
+const char* DEFAULT_SSID     = "YOUR_WIFI_NAME";
+const char* DEFAULT_PASSWORD = "YOUR_WIFI_PASSWORD";
+
+const char* BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN";
+const char* CHAT_ID   = "YOUR_TELEGRAM_CHAT_ID";
